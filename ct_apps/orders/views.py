29eefc39dd9e416945cdc8742e660eb7fc11a7a7
Bruna <brn.flor@gmail.com>
2025-03-30 13:08:45 +0100

    Creating some urls to detail views

--- ct_apps/orders/views.py
+++ ct_apps/orders/views.py
@@ -1,43 +1,52 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views import View
 
 
 # Create your views here.
-def index(request):
-    context = {
-        "user": {
-            "name": "Wes",
-            "type":"consumer"
-        },
-        "sale_order": {
-            "id": '00123',
-            "items": [
+class SaleOrdersView(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'orders/index.html', context)
+
+
+class SaleOrdersDetailView(View):
+    def get(self, request, id):
+        context = {
+            "id": "00123",
+            "user": "Wes",
+            "purchased_items": [
                 {
-                    "id": "#123456",
+                    "id": "123456",
                     "amount": 3,
                     "name": 'Feijoada',
                     "price": 10.00
                 },
                 {
-                    "id": "#234567",
+                    "id": "234567",
                     "amount": 5,
                     "name": 'Cocola',
                     "price": 1.00
                 },
                 {
-                    "id": "#34567",
+                    "id": "34567",
                     "amount": 3,
                     "name": 'Sagu',
                     "price": 1.50
                 },
                 {
-                    "id": "#4567",
+                    "id": "4567",
                     "amount": 3,
                     "name": 'Canjica',
                     "price": 0.80
                 }
+            ],
+            "retrieved_items": [
+                {
+                    "id": "#4567",
+                    "name": 'Canjica',
+                    "timestamp": "date-time"
+                }
             ]
         }
-
-    }
-    return render(request, 'orders/index.html', context)
+        return render(request, 'orders/roles/consumer.html', context)

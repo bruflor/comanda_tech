82f7d72fd36d2bc92d6a14bf08067fe11232e6f9
Wes Benvindo <wal2.designer@gmail.com>
2025-03-31 20:58:25 +0100

    Adding order functionality

--- ct_apps/orders/admin.py
+++ ct_apps/orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from ct_apps.orders.models import OrderSale, OrderItem, Product
+
+
 # Register your models here.
+@admin.register(OrderSale, OrderItem, Product)
+class OrderSaleAdmin(admin.ModelAdmin):
+    pass
+
+
+# @admin.register(OrderItem)
+# class OrderItem(admin.AdminSite):
+#     pass
+
+
+# @admin.register(Product)
+# class ProductAdmin(admin.AdminSite):
+#     pass
--- ct_apps/orders/models.py
+++ ct_apps/orders/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 
 # Create your models here.
-class SaleOrder(models.Model):
+class OrderSale(models.Model):
     consumer = models.CharField(max_length=200, default="", null=True, blank=True)
-    purchased_items = model
+    
+    def __str__(self):
+        return f"{self.consumer}"
 
 
-class AppUser(models.Model):
-    pass
+class OrderItem(models.Model):
+    item = models.ForeignKey("Product", on_delete=models.CASCADE)
+    order = models.ForeignKey("OrderSale", on_delete=models.CASCADE, related_name='purchased_item')
+    amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.order.consumer} : {self.item.name} "
 
 
 class Product(models.Model):
@@ -16,3 +23,6 @@ class Product(models.Model):
     price = models.FloatField(null=False, blank=False, default=0.0)
     stock_unity = models.IntegerField(null=False, blank=False, default=1)
     cost_per_unity = models.FloatField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.name}"

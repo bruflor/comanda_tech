1c86a5810f3d7d091aa7ce9aed625225c9826567
Wes Benvindo <wal2.designer@gmail.com>
2025-04-01 21:22:24 +0100

    Adding product and connecting with database

--- ct_apps/orders/forms.py
+++ ct_apps/orders/forms.py
@@ -4,6 +4,6 @@ from ct_apps.orders.models import Product
 
 
 class ProductForm(forms.Form):
-    products_choice = [(p.id, p.name, p.price, p.stock_unity) for p in Product.objects.all()]
+    products_choice = [(p.id, p.name, p.price, p.stock_unity) for p in Product.objects.all().order_by('name')]
     
     Item = forms.ChoiceField(choices=products_choice)

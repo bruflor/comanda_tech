419a62d8d09dc5c85fcf0ffd57928fd88f64e42c
Wes Benvindo <wal2.designer@gmail.com>
2025-04-06 16:27:29 +0100

    Remove stock_unity zero from been selected

--- ct_apps/orders/forms.py
+++ ct_apps/orders/forms.py
@@ -4,6 +4,7 @@ from ct_apps.orders.models import Product
 
 
 class ProductForm(forms.Form):
-    products_choice = [(p.id, p.name, p.price, p.stock_unity) for p in Product.objects.all().order_by('name')]
-    
+    products_choice = [(p.id, p.name, p.price, p.stock_unity) for p in
+                       Product.objects.filter(stock_unity__gt=0).order_by('name')]
+
     Item = forms.ChoiceField(choices=products_choice)

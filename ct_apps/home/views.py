48ba93e11f8e15afdddbb2e430209fb919cb19f6
Wes Benvindo <wal2.designer@gmail.com>
2025-04-12 13:24:18 +0100

    adding menu

--- ct_apps/home/views.py
+++ ct_apps/home/views.py
@@ -1,15 +1,20 @@
 # Create your views here.
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
-from ct_apps.orders.models import OrderSale
+from ct_apps.orders.models import OrderSale, Product
 
 
 class HomeView(View):
     def get(self, request, order_id=None):
-        context = {}
+
+        products = Product.objects.filter(sold_unity__lt=F("initial_stock")).exclude(is_internal=True).order_by(
+            'name')
+
+        context = {"products": products}
 
         if request.GET.get('search_order_id'):
             order_id = request.GET.get('search_order_id')
@@ -24,8 +29,9 @@ class HomeView(View):
                 context = {
                     "order_sale": order,
                     "purchased_items": purchased_item,
+
                 }
-                
+
             except ObjectDoesNotExist:
                 pass
 
